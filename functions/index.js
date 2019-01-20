924bf0c818747b6c0fdeff8a7a121b9f3779cbb9
Jessica Zanelato <jessizanelato@gmail.com>
2019-01-20 21:15:29 -0200

    Adicionada funcao que retorna todos os inscritos ativos

--- functions/index.js
+++ functions/index.js
@@ -18,6 +18,7 @@ exports.picpay = functions.https.onRequest((request, response) => {
                 id: body.event.subscriber.id,
                 name: body.event.subscriber.name,
                 email: body.event.subscriber.email,
+                username: body.event.subscriber.username,
                 plan_name: body.event.plan.name,
                 plan_value: body.event.plan.value,
                 inserted_date: body.date,
@@ -37,12 +38,28 @@ exports.picpay = functions.https.onRequest((request, response) => {
             }).catch(error => {
                 response.status(500).send("Subscriber doesn't exist.");
             });
-            
+
             break;
-    
+
         default:
             response.status(501).send("Not implemented");
             break;
     }
-    
+
+});
+
+exports.picpayActiveList = functions.https.onRequest((request, response) => {
+    db.collection('subscribers').where('status', '=', 'active')
+    .get()
+    .then(function(query) {
+        let data = {};
+        query.forEach(function(doc) {
+            data[doc.id] = doc.data();
+        });
+        response.send(data);
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+
 });
--- functions/parsing-json.js
+++
@@ -1,8 +0,0 @@
-const Firestore = require('@google-cloud/firestore');
-
-const firestore = new Firestore({
-    projectId: 'YOUR_PROJECT_ID',
-    keyFilename: '/path/to/keyfile.json',
-  });
-
-  // add papaparse json para converter o arquivo e salvar no banco de dados
